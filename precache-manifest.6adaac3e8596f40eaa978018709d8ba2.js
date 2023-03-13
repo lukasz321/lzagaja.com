146ff935fdc7c00fd32cb2af233fe3aafb66f49c
Lukasz Zagaja <lu.zagaja@gmail.com>
2023-03-12 20:11:08 -0400

    Updates

---
+++ precache-manifest.6adaac3e8596f40eaa978018709d8ba2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "cda3a5d31a28bf31101c",
+    "url": "/static/css/main.e8fc7118.chunk.css"
+  },
+  {
+    "revision": "cda3a5d31a28bf31101c",
+    "url": "/static/js/main.cda3a5d3.chunk.js"
+  },
+  {
+    "revision": "d0acc314c0a6c805f44b",
+    "url": "/static/js/1.d0acc314.chunk.js"
+  },
+  {
+    "revision": "3d40d30cdba33a5a639c",
+    "url": "/static/js/2.3d40d30c.chunk.js"
+  },
+  {
+    "revision": "a3b6dd91a2de68d25800",
+    "url": "/static/js/runtime~main.a3b6dd91.js"
+  },
+  {
+    "revision": "a5b16fceed5db8316a66187b02c40d08",
+    "url": "/index.html"
+  }
+];
